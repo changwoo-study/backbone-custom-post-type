120f943f6526f8ab6f61cf0388562f192e03439c
changwoo <ep6tri@hotmail.com>
2017-08-14 21:20:23 +0900

    bbcpt 슬러그를 변경.

--- functions.php
+++ functions.php
@@ -1,26 +1,26 @@
 <?php
 
-function &bbcpt_memory() {
+function &bb_cpt_memory() {
 	static $memory = array();
 
 	return $memory;
 }
 
-function bbcpt_memory_set( $key, $value ) {
-	$memory         = &bbcpt_memory();
+function bb_cpt_memory_set( $key, $value ) {
+	$memory         = &bb_cpt_memory();
 	$memory[ $key ] = $value;
 }
 
-function bbcpt_memory_get( $key, $default = FALSE ) {
-	$memory = &bbcpt_memory();
+function bb_cpt_memory_get( $key, $default = FALSE ) {
+	$memory = &bb_cpt_memory();
 
 	return isset( $memory[ $key ] ) ? $memory[ $key ] : $default;
 }
 
-function bbcpt_template( $template_name, $context = array() ) {
+function bb_cpt_template( $template_name, $context = array() ) {
 	$template = __DIR__ . '/templates/' . trim( $template_name, '/' );
 	if ( ! realpath( $template ) ) {
-		wp_die( sprintf( __( 'template \'%s\' does not exist.', 'bbcpt' ), $template ) );
+		wp_die( sprintf( __( 'template \'%s\' does not exist.', 'bb-cpt' ), $template ) );
 	}
 	if ( $context ) {
 		extract( $context );
